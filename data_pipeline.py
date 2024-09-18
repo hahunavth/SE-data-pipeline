b8401123028369638dfd29667a56420857fabddc
hahunavth <vuthanhha.2001@gmail.com>
2024-09-18 17:20:13 +0700

    feat: update verbose

--- data_pipeline.py
+++ data_pipeline.py
@@ -598,6 +598,7 @@ def process_channel(idx, row, min_snr, min_ac_speech_prob, log_queue, repo_id=No
 
                 if audio_path is not None:
                     # channel_audio_paths.append(audio_path)
+                    _log_queue_put("SKIP_AUDIO_PATH_NONE")
                     continue
             except Exception as e:
                 if "PREMIERE_VIDEO" in str(e) or "OFFLINE_VIDEO" in str(e):
