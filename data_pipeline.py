f0fbeec1e4b26673649d9583f5ca1114822e5cf6
hahunavth <vuthanhha.2001@gmail.com>
2024-09-21 09:27:26 +0700

    feat: update n_video_download

--- data_pipeline.py
+++ data_pipeline.py
@@ -520,15 +520,15 @@ def process_channel(idx, row, min_snr, min_ac_speech_prob, log_queue, repo_id=No
     if channel_n_sub < 10000:
         n_video_download = 10
     elif channel_n_sub < 30000:
-        n_video_download = 30
+        n_video_download = 20
     elif channel_n_sub < 50000:
-        n_video_download = 50
+        n_video_download = 30
     elif channel_n_sub < 100000:
-        n_video_download = 60
+        n_video_download = 40
     elif channel_n_sub < 200000:
-        n_video_download = 70
+        n_video_download = 50
     else:
-        n_video_download = 90
+        n_video_download = 60
 
     def _log_queue_put(level=logging.INFO, msg=""):
         log_queue.put(
